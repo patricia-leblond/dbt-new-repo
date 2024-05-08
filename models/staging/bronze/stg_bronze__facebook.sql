d8ef49484c9c2a02ee90f46f41f0e54b43b2ab01
patriciacleblond@gmail.com <patriciacleblond@gmail.com>
2024-05-08 15:26:18 +0000

    cast ads_cost as float in all media bronze tables

--- models/staging/bronze/stg_bronze__facebook.sql
+++ models/staging/bronze/stg_bronze__facebook.sql
@@ -13,7 +13,7 @@ renamed as (
         paid_source,
         campaign_key,
         campgn_name AS campaign_name,
-        ads_cost,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
         impression,
         click
 
